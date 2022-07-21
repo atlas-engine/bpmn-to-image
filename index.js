144976f40da812d0c35647601c1f1c0993711ed5
Martin Moellenbeck <Martin.Moellenbeck@5Minds.de>
2022-07-21 17:12:31 +0200

    Add --no-sandbox to puppeter

--- index.js
+++ index.js
@@ -112,7 +112,9 @@ async function withPage(fn) {
   let browser;
 
   try {
-    browser = await puppeteer.launch();
+    browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
 
     await fn(await browser.newPage());
   } finally {
